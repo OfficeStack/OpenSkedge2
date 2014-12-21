dc9feae2da3040414081528dacab3a327b45ebf0
Max Fierke <max@maxfierke.com>
2014-12-21 00:48:50 -0600

    Add paradigm validator to Position
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,12 +1,19 @@
 class Position < ActiveRecord::Base
+  # Constants
+  PARADIGM_AVAILABILITY = 'availability'
+  PARADIGM_FIXED_SHIFT = 'fixed_shift'
+  PARADIGM_NO_SHIFT = 'no_shift'
+  SUPPORTED_PARADIGMS = [PARADIGM_AVAILABILITY, PARADIGM_FIXED_SHIFT, PARADIGM_NO_SHIFT]
+
+  # Relations
   belongs_to :group
-
   has_many :shifts
-
   has_many :user_positions
   has_many :users, through: :user_positions
 
+  # Validators
   validates :name, presence: true, length: { maximum: 75 }
   validates :description, length: { maximum: 1000 }
-  validates :group_id, presence: true
+  validates :group, presence: true
+  validates :paradigm, presence: true, inclusion: { in: SUPPORTED_PARADIGMS }
 end
