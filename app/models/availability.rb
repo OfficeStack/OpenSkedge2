2591490175c8f3e8b6e525409480ae5b3541c3e7
Max Fierke <max@maxfierke.com>
2014-12-21 00:49:19 -0600

    Add preference_level AR validator to Availability
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,7 +1,16 @@
 class Availability < ActiveRecord::Base
+  # Constants
+  LOW_PREFERENCE = 1
+  MED_PREFERENCE = 2
+  HIGH_PREFERENCE = 3
+
+  # Relations
   belongs_to :user
 
-  validates :user_id, presence: true
+  # Validators
+  validates :user, presence: true
+  validates :preference_level, presence: true, numericality: { only_integer: true },
+    inclusion: { in: LOW_PREFERENCE..HIGH_PREFERENCE }
   validates :start_time, presence: true, date: { before: :end_time }
   validates :end_time, presence: true, date: { after: :start_time }
   # TODO: Recurrence validator
