9229e5c66ecf9e81a07f1f38264c2e10be4c199c
Bryce <br.kalow@gmail.com>
2014-12-29 19:00:25 -0600

    Added initial position spec

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -1,5 +1,17 @@
-require 'rails_helper'
+  require 'rails_helper'
 
 RSpec.describe Position, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:group) }
+    it { should have_many(:shifts) }
+    it { should have_many(:user_positions) }
+    it { should have_many(:users).through(:user_positions) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should ensure_length_of(:name).is_at_most(75) }
+    it { should ensure_length_of(:description).is_at_most(1000) }
+    it { should validate_presence_of(:group) }
+  end
 end
