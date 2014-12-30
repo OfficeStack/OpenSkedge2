d3404a54f24fe6194a58929dab216e0ed3ef6044
Bryce <br.kalow@gmail.com>
2014-12-29 19:10:03 -0600

    Added initial timecard_punch spec

--- spec/models/timecard_punch_spec.rb
+++ spec/models/timecard_punch_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TimecardPunch, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:user) }
+    it { should validate_presence_of(:start_datetime) }
+  end
 end
